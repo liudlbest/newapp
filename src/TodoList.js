1d6c5a3a6a502307f22953f6014f0d0e4c0f5304
liudl <liudlbest@126.com>
2020-01-09 00:01:18 +0800

    todoList init 2 handleInputChange method

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,15 +1,36 @@
 import React, {Fragment} from 'react'
 
-function TodoList() {
-  return (
-    <Fragment>
-      <div><input/><button>submit</button></div>
-      <ul>
-        <li>学英语</li>
-        <li>Learning React</li>
-      </ul>
-    </Fragment>
-  )
+class TodoList extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      inputValue: '',
+      list: []
+    }
+  }
+
+  handleInputChange(e){
+    console.log(e.target.value);
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+
+  render(){
+    return (
+      <Fragment>
+        <div>
+          <input value={this.state.inputValue} onChange={this.handleInputChange.bind(this)}/>
+          <button>submit</button>
+        </div>
+        <ul>
+          <li>学英语</li>
+          <li>Learning React</li>
+        </ul>
+      </Fragment>
+    )
+  }
 }
 
 export default TodoList
